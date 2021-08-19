9f743bdfb918b64299778af5c4302b1406ae31b6
_Lucas_ <lucasudar@gmail.com>
2021-08-19 23:23:11 +0300

    done with html, css, js

--- app.js
+++ app.js
@@ -1,16 +1,55 @@
 $(function () {
     let header = $("#header")
     let intro = $("#intro")
-    let scrollPos
+    let scrollPos = $(window).scrollTop()
     let introH = intro.innerHeight()
-    $(window).on("scroll load resize", function () {
+    let nav = $("#nav")
+    let navToggle = $("#navToggle")
+
+    checkScroll(scrollPos, introH)
+
+    $(window).on("scroll resize", function () {
         introH = intro.innerHeight()
         scrollPos = $(this).scrollTop()
 
+        checkScroll(scrollPos, introH)
+    });
+    function checkScroll(scrollPos, introH) {
         if (scrollPos > introH) {
             header.addClass("fixed")
         } else {
             header.removeClass("fixed")
         }
+    }
+
+    $("[data-scroll]").on("click", function (event) {
+        event.preventDefault()
+
+        let elementId = $(this).data("scroll")
+        let elementOffset = $(elementId).offset().top
+
+        nav.removeClass("show")
+
+        $("html, body").animate({
+            scrollTop: elementOffset - 60
+        }, 700)
     })
+
+
+    navToggle.on("click", function (event) {
+        event.preventDefault()
+
+        nav.toggleClass("show")
+    })
+
+    let slider = $("#reviewsSlider")
+
+    slider.slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: true,
+        arrows: false,
+        dots: true,
+    });
 })
